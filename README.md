1af424265c693f40c3c399f33229fc8bb5c09c6d
Dalia <umme.fatima@gmail.com>
2018-03-10 22:11:47 +0100

    updated

---
+++ README.md
@@ -0,0 +1,44 @@
+Neighborhood Map
+================
+This project is a single page application that displays few of the tourist spots around Oslo, Norway.
+
+Design
+=======
+## 1. This projects makes use of following
+      - HTML
+       -CSS
+       -Bootsrap
+       -JavaScript
+       -JQuery
+       -KnockOut JS
+
+## 2. The APIs used are
+       * Google Maps
+       * Wikipedia
+
+Pre-Requirements
+=================
+-  1. A web browser, preferably Google Chrome.
+-  2. Create a githib account and Install git in the local machine. Click [here](https://github.com/), if 
+      not already installed
+
+Installation Steps
+==================
+-  1. Fork the github repository. The original repository can be found [here](https://github.com/daliasgit/NeighborhoodMap)
+-  2. Open Terminal
+-  3. Clone it to local repository using '''git clone <fork repository url>'''
+-  4. Run the command '''cd/NeighborhoodMap'''
+
+FAQ
+====
+ - 1. How do I run the project?
+ - 2. Open the project folder
+ - 3. Right Click '''index.html''' and open with google chrome (or any available browser)
+
+Reference
+==========
+   1. [KnockOut JS](http://knockoutjs.com/index.html)
+   2. [stackoverflow](https://stackoverflow.com/)
+   3. [W3Schools](https://www.w3schools.com/)
+   4. [Google Maps API](https://developers.google.com/maps/)
+   5. [WikiMedia](https://www.mediawiki.org/wiki/API:Tutorial)
--- js/map.js
+++ js/map.js
@@ -4,8 +4,7 @@ var marker;
 var infowindow;
 // Create a new blank array for all the listing markers.
 var markers = [];
-// This global polygon variable is to ensure only ONE polygon is rendered.
-var polygon = null;
+
 // These are the place listings that wiil be shown to the user.
 var locations = [ {
 	name: 'The Vigeland Park Oslo',
@@ -45,8 +44,8 @@ var locations = [ {
 		lng: 10.778209
 	}
 } ];
-function initMap() {
-    // Create a styles array to use with the map.
+
+ // Create a styles array to use with the map.
     var styles = [
        {
         featureType: 'water',
@@ -113,226 +112,140 @@ function initMap() {
         ]
         }
     ];
-	// Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: 59.927278, lng: 10.700805},
-    	zoom: 13,
-        styles: styles,
-        mapTypeControl: false
-	});
 
-	var largeInfowindow = new google.maps.InfoWindow();
-    // Initialize the drawing manager.
-    var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_LEFT,
-            drawingModes: [
-              google.maps.drawing.OverlayType.POLYGON
-            ]
-        }
-    });
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
+var Location = function( data ) {
+    this.name = ko.observable( data.name );
+    this.visible = ko.observable( true );
+    this.location = ko.observable( data.location );
+    this.marker = data.marker;
+};
 
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
+var ViewModel = function() {
+    var self = this;
+    this.myLocations = ko.observableArray( [] );
+    locations.forEach( function( locationItem ) {
+        self.myLocations.push( new Location( locationItem ) );
+    } );
+    this.currentlocation = ko.observable( this.myLocations()[ 0 ] );
+    this.markers = ko.observableArray( locations );
+    this.filter = ko.observable( "" );
+    this.wikipedia = ko.observableArray( [] );
+    url = ko.observable( this.currentlocation );
+    details = ko.observable( url );
 
-      
+    this.search = function( value ) {
+        // remove all the current locations, which removes them from the view
+        if ( value ) {
+            // filter the markers with the Marker setVisible method
+            for ( var x in locations ) {
+                if ( locations[ x ].name.toLowerCase()
+                    .indexOf( value.toLowerCase() ) >= 0 ) {
+                    self.myLocations()[ x ].visible( true );
+                    self.myLocations()[ x ].marker.setVisible( true );
 
-    // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < locations.length; i++) {
-        // Get the position from the location array.
-        var position = locations[i].location;
-        var title = locations[i].name;
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-          });
-        // Push the marker to our array of markers.
-        markers.push(marker);
-        // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-        });
-        // Two event listeners - one for mouseover, one for mouseout,
-        // to change the colors back and forth.
-        marker.addListener('mouseover', function() {
-          this.setIcon(highlightedIcon);
-        });
-        marker.addListener('mouseout', function() {
-          this.setIcon(defaultIcon);
-        });
-        
-    }
-    
-    document.getElementById('show-listings').addEventListener('click', showListings);
-    document.getElementById('hide-listings').addEventListener('click', hideListings);
-
-    document.getElementById('toggle-drawing').addEventListener('click', function() {
-        toggleDrawing(drawingManager);
-    });
-
-    document.getElementById('zoom-to-area').addEventListener('click', function() {
-        zoomToArea();
-    });
-
-    // Add an event listener so that the polygon is captured,  call the
-    // searchWithinPolygon function. This will show the markers in the polygon,
-    // and hide any outside of it.
-    drawingManager.addListener('overlaycomplete', function(event) {
-        // First, check if there is an existing polygon.
-        // If there is, get rid of it and remove the markers
-        if (polygon) {
-            polygon.setMap(null);
-            hideListings(markers);
-        }
-        // Switching the drawing mode to the HAND (i.e., no longer drawing).
-        drawingManager.setDrawingMode(null);
-        // Creating a new editable polygon from the overlay.
-        polygon = event.overlay;
-        polygon.setEditable(true);
-        // Searching within the polygon.
-        searchWithinPolygon();
-        // Make sure the search is re-done if the poly is changed.
-        polygon.getPath().addListener('set_at', searchWithinPolygon);
-        polygon.getPath().addListener('insert_at', searchWithinPolygon);
-    });
-      
-}
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
-        infowindow.setContent('');
-        infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-        infowindow.marker = null;
-        });
-        var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-        // In case the status is OK, which means the pano was found, compute the
-        // position of the streetview image, then calculate the heading, then get a
-        // panorama from that and set the options
-        function getStreetView(data, status) {
-            if (status == google.maps.StreetViewStatus.OK) {
-              var nearStreetViewLocation = data.location.latLng;
-              var heading = google.maps.geometry.spherical.computeHeading(
-                nearStreetViewLocation, marker.position);
-                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                var panoramaOptions = {
-                  position: nearStreetViewLocation,
-                  pov: {
-                    heading: heading,
-                    pitch: 30
-                  }
-                };
-              var panorama = new google.maps.StreetViewPanorama(
-                document.getElementById('pano'), panoramaOptions);
-            } else {
-              infowindow.setContent('<div>' + marker.title + '</div>' +
-                '<div>No Street View Found</div>');
+                } else {
+                    self.myLocations()[ x ].visible( false );
+                    self.myLocations()[ x ].marker.setVisible( false );
+                }
             }
-          }
-          // Use streetview service to get the closest streetview image within
-          // 50 meters of the markers position
-          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-          // Open the infowindow on the correct marker.
-
-          infowindow.open(map, marker);
-        
-    }
-}
-// This function will loop through the markers array and display them all.
-function showListings() {
-   var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
-    }
-    map.fitBounds(bounds);
-}
-
-// This function will loop through the listings and hide them all.
-function hideListings() {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-}
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-       new google.maps.Size(21, 34),
-       new google.maps.Point(0, 0),
-       new google.maps.Point(10, 34),
-       new google.maps.Size(21,34));
-    return markerImage;
-}
-// This shows and hides (respectively) the drawing options.
-function toggleDrawing(drawingManager) {
-    if (drawingManager.map) {
-        drawingManager.setMap(null);
-        // In case the user drew anything, get rid of the polygon
-        if (polygon !== null) {
-            polygon.setMap(null);
+        } else {
+            for ( var y in locations ) {
+                if ( locations[ y ].name.toLowerCase()
+                    .indexOf( value.toLowerCase() ) >= 0 )
+                    self.myLocations()[ y ].visible( true );
+                    self.myLocations()[ y ].marker.setVisible( true );
+            }
         }
-      } else {
-          drawingManager.setMap(map);
-      }
-}
+    };
+    this.initMap = function() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map( document.getElementById( 'map' ), {
+            center: {lat: 59.927278, lng: 10.700805},
+            zoom: 13,
+            styles: styles,
+            mapTypeControl: false
+        } );
 
-// This function hides all markers outside the polygon,
-// and shows only the ones within it. This is so that the
-// user can specify an exact area of search.
-function searchWithinPolygon() {
-    for (var i = 0; i < markers.length; i++) {
-        if (google.maps.geometry.poly.containsLocation(markers[i].position, polygon)) {
-            markers[i].setMap(map);
-      } else {
-            markers[i].setMap(null);
+        infowindow = new google.maps.InfoWindow();
+        // The following group uses the location array to create an array of markers on initialize.
+        for ( var i = 0; i < locations.length; i++ ) {
+            // Get the position from the location array.
+            var position = locations[ i ].location;
+            var title = locations[ i ].name;
+            // Create a marker per location, and put into markers array.
+            marker = new google.maps.Marker( {
+                map: map,
+                position: position,
+                title: title,
+                animation: google.maps.Animation.DROP,
+                id: i
+            } );
+            // Push the marker to our array of markers.
+            markers.push( marker );
+
+            marker.addListener( 'click', self.populateInfoWindow );
+
+            // Push the marker to our array of markers. 
+            self.myLocations()[ i ].marker = marker;
         }
-    }
-}
-// This function takes the input value in the find nearby area text input
-// locates it, and then zooms into that area. This is so that the user can
-// show all listings, then decide to focus on one area of the map.
-function zoomToArea() {
-    // Initialize the geocoder.
-    var geocoder = new google.maps.Geocoder();
-    // Get the address or place that the user entered.
-    var address = document.getElementById('zoom-to-area-text').value;
-    // Make sure the address isn't blank.
-    if (address == '') {
-        window.alert('You must enter an area, or address.');
-    } else {
-        // Geocode the address/area entered to get the center. Then, center the map
-        // on it and zoom in
-        geocoder.geocode(
-            { address: address,
-              componentRestrictions: {locality: 'New York'}
-            }, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                map.setZoom(15);
-              } else {
-                window.alert('We could not find that location - try entering a more' +
-                    ' specific place.');
-              }
-            });
-    }
-}
+    };
+
+    this.populateInfoWindow = function() {
+        var marker = this;
+        loadData( marker.title );
+        // Check to make sure the infowindow is not already opened on this marker.
+        if ( infowindow.marker != marker ) {
+            infowindow.marker = marker;
+            infowindow.setContent( '<div>' + marker.title + '</div>' );
+            marker.setAnimation( google.maps.Animation.BOUNCE ); // Bounce marker when list view item is clicked
+            //infowindow.open(map, place.marker); // Open info window on correct marker when list item is clicked
+            setTimeout( function() {
+                marker.setAnimation( null ); // End animation on marker after 2 seconds
+            }, 2000 );
+            infowindow.open( map, marker );
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener( 'closeclick', function() {
+                infowindow.marker = null;
+            } );
+        }
+    };
+    //Click on item in list view
+    this.listViewClick = function( place ) {
+        google.maps.event.trigger( place.marker, 'click' );
+    };
+};
+
+function mapError() {
+    document.getElementById("head").innerHTML = "<h1>Oops! Something went wrong. Please try again!</h1>";
+};
+
+var loadData = function( name ) {
+
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + name + '&format=json&callback=wikiCallback';
+
+    $.ajax( {
+        url: wikiUrl,
+        dataType: "jsonp", // jsonp: "callback",
+        success: function( response ) {
+            viewModel.wikipedia( [] );
+            console.log( response );
+            var articleList = response[ 1 ];
+            for ( var i = 0; i < articleList.length; i++ ) {
+                articleStr = articleList[ i ];
+                var url = 'https://en.wikipedia.org/wiki/' + articleStr;
+
+                viewModel.wikipedia.push( url );
+            }
+        }
+    }).fail(function (jqXHR, textStatus) {
+        alert( "failed to get wikipedia resources" );
+    });
+};
+
+           $("#menu-toggle").click(function(e) {
+               e.preventDefault();
+               $("#wrapper").toggleClass("toggled");           
+           });
+var viewModel = new ViewModel();
+viewModel.filter.subscribe( viewModel.search );
+ko.applyBindings( viewModel );
